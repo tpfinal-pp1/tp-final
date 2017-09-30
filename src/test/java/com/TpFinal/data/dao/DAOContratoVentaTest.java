21049d21cfa9fb2d32ef2aa3a39c6dfe6483046f
Agus Alexander <agusalexander@hotmail.com>
2017-09-30 19:27:32 -0300

    uff ahora si fix

--- src/test/java/com/TpFinal/data/dao/DAOContratoVentaTest.java
+++ src/test/java/com/TpFinal/data/dao/DAOContratoVentaTest.java
@@ -83,13 +83,14 @@ public class DAOContratoVentaTest {
 		
 	}
 	
-	/*@Test
+	@Test
 	public void altaConDoc() throws SQLException, IOException {
 		String path="Files"+File.separator+"demo1.pdf";
 		File f=new File(path);
 		f.createNewFile();  //lo creo
 		persistirenDB(path); //Lo guardo en db
 		f.delete();          //Lo borro del disco
+		assertFalse(f.exists());
 		assertEquals(1, dao.readAll().size());
 		ContratoVenta contrato = dao.readAll().get(0); //Lo traigo de DB
 		guardar(path+".test", toBytes(contrato));  //Lo escribo en disco de nuevo
@@ -97,7 +98,7 @@ public class DAOContratoVentaTest {
 		f=new File(path);
 		assertTrue(f.exists());
 
-	}*/
+	}
 	
 	public void persistirenDB(String path) throws FileNotFoundException {
 		File pdf = new File(path);
--- src/test/java/com/TpFinal/services/ContratoVentaServiceTest.java
+++ src/test/java/com/TpFinal/services/ContratoVentaServiceTest.java
@@ -3,6 +3,7 @@ package com.TpFinal.services;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,14 +19,12 @@ import com.TpFinal.data.dto.contrato.ContratoVenta;
 public class ContratoVentaServiceTest {
 	ContratoVentaService service;
 	List<ContratoVenta>contratos=new ArrayList<>();
-	
+
 	@Before
 	public void setUp() throws Exception {
-		File f = new File("src\\main\\resources\\prueba.pdf");
-		if(f.exists())
-			f.delete();
-		else
-			System.out.println("no existe");
+		File dir=new File("Files");
+		deleteDirectory(dir);
+		dir.mkdir();
 		service = new ContratoVentaService();
 		contratos.clear();
 	}
@@ -34,22 +33,33 @@ public class ContratoVentaServiceTest {
 	public void tearDown() throws Exception {
 		contratos=service.readAll();
 		contratos.forEach(c -> service.delete(c));
-		File f = new File("src\\main\\resources\\prueba.pdf");
-		if(f.exists())
-			f.delete();
-		else
-			System.out.println("no existe");
+		File f = new File("Files");
+		deleteDirectory(f);
 	}
 
-	@Test
-	public void test() {
-		File pdf = new File("src\\main\\resources\\demo.pdf");
+	static public boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+@Test
+	public void test() throws IOException {
+		File pdf = new File("Files"+File.separator+"demo1.pdf");
+		pdf.createNewFile();
 		assertTrue(pdf.exists());
 		service.save(instancia("1"), pdf);
 		ContratoVenta c = service.readAll().get(0);
 		ContratoUtil cu = new ContratoUtil();
-		cu.downloadFile(c, "src\\main\\resources\\prueba.pdf");
-		pdf=new File("src\\main\\resources\\prueba.pdf");
+		cu.downloadFile(c, "Files"+File.separator+"demo1.pdf");
+		pdf=new File("Files"+File.separator+"demo1.pdf");
 		assertTrue(pdf.exists());
 		pdf.delete();
 	}
