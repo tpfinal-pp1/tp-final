bdb12e8a35da8fe57dff300212d01c897fdf4a38
Agus Alexander <agusalexander@hotmail.com>
2017-10-02 23:55:26 -0300

    fix ahora si ?

--- src/test/java/com/TpFinal/selenium/tests/SeleniumTest.java
+++ src/test/java/com/TpFinal/selenium/tests/SeleniumTest.java
@@ -1,8 +1,11 @@
 package com.TpFinal.selenium.tests;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
 
 /**
  * Created by Max on 10/2/2017.
@@ -11,7 +14,14 @@ public class SeleniumTest {
 
     @Test
     public void seleniumTest(){
-        System.setProperty("webdriver.gecko.driver", "C:\\Drivers\\geckodriver.exe");
+        if(SystemUtils.IS_OS_LINUX){
+            System.setProperty("webdriver.gecko.driver","GeckoDriver"+ File.separator+"geckodriver");
+            System.out.println("OS: LINUX");
+        }
+        if(SystemUtils.IS_OS_WINDOWS) {
+            System.setProperty("webdriver.gecko.driver", "GeckoDriver"+ File.separator+"geckodriver.exe");
+            System.out.println("OS: Windows");
+        }
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("http://inmobi.ddns.net/");
         webDriver.close();
