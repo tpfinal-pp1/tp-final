2c497167e93674cba91332525683831392ded540
deidelson <deidelson@mail.com>
2017-10-06 16:03:15 -0300

    push

--- src/test/java/com/TpFinal/services/AllTests.java
+++ src/test/java/com/TpFinal/services/AllTests.java
@@ -7,5 +7,4 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({PersonaServiceNuevoTest.class, ContratoServiceTest.class, ProvinciaServiceTest.class })
 public class AllTests {
-
 }
