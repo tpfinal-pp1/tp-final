95494f2b3cc0e253d6a6e81a632e3ba31eaf84ea
deidelson <deidelson@mail.com>
2017-10-06 15:38:22 -0300

    Service de provincias

---
+++ src/main/java/com/TpFinal/services/ProvinciaService.java
@@ -0,0 +1,30 @@
+package com.TpFinal.services;
+
+import java.util.List;
+
+import com.TpFinal.data.dto.JsonUtil;
+import com.TpFinal.data.dto.LocalidadRAW;
+import com.TpFinal.data.dto.Provincia;
+
+public class ProvinciaService {
+	private JsonUtil json;
+	private String path;
+	private List<Provincia>provincias;
+	
+	public ProvinciaService(String pathJson) {
+		path=pathJson;
+		json=new JsonUtil();
+		getProvinciasJson();
+	}
+	
+	public List<Provincia> getProvincias(){
+		return this.provincias;
+	}
+	
+	private void getProvinciasJson(){
+		List<LocalidadRAW>localidades=json.leerRaw("src\\main\\webapp\\Localidades.json");
+		this.provincias=json.rawToProvincias(localidades);
+		
+	}
+
+}
