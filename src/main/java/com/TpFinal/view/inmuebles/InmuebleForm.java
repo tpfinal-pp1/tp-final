95783a954b9295c56516608982ec68d049d4f6fc
roberGzt <rober.gerszen@gmail.com>
2017-10-06 21:21:37 -0300

    fixed relacion entre combos de localidades y provincias

--- src/main/java/com/TpFinal/view/inmuebles/InmuebleForm.java
+++ src/main/java/com/TpFinal/view/inmuebles/InmuebleForm.java
@@ -116,11 +116,11 @@ public class InmuebleForm extends FormLayout {
 		    if (valueChangeEvent.getOldValue() != null) {
 			localidades.clear();
 			localidades.setItems(provincia.getLocalidades());
-			if (inmueble.getDireccion().getLocalidad() == "") {
-			    localidades.setSelectedItem(provincia.getLocalidades().get(0));
-			} else {
+			if (inmueble.getDireccion().getProvincia() == provincia.getNombre()) {
 			    localidades.setSelectedItem(provinciaService.getLocalidadFromNombreAndProvincia(inmueble
 				    .getDireccion().getLocalidad(), inmueble.getDireccion().getProvincia()));
+			}else {
+			    localidades.setSelectedItem(provincia.getLocalidades().get(0));
 			}
 		    }
 		}
