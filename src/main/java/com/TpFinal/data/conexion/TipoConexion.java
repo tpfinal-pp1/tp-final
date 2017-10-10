64519860ad4736ca06260a3f22bd0d57d5d1eec0
roberGzt <rober.gerszen@gmail.com>
2017-10-10 11:30:13 -0300

    vuelta a modo update

--- src/main/java/com/TpFinal/data/conexion/TipoConexion.java
+++ src/main/java/com/TpFinal/data/conexion/TipoConexion.java
@@ -17,7 +17,7 @@ public enum TipoConexion {	H2Test("org.hibernate.dialect.H2Dialect"
 									,"root"
 									,"root"
 									,"false"
-									,"create-drop"),
+									,"update"),
 							MariaDBTest("org.hibernate.dialect.MariaDB53Dialect"
 									,"org.mariadb.jdbc.Driver"
 									,"jdbc:mysql://localhost:3306/prueba"
