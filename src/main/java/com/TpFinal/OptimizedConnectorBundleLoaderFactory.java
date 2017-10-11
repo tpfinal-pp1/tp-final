ee0c96b7751909b7287fa9650855010908ace2aa
Agus Alexander <agusalexander@hotmail.com>
2017-10-11 17:14:28 -0300

    widgetset optimizado

--- src/main/java/com/TpFinal/OptimizedConnectorBundleLoaderFactory.java
+++ src/main/java/com/TpFinal/OptimizedConnectorBundleLoaderFactory.java
@@ -1,40 +1,43 @@
 package com.TpFinal;
 
+import java.util.HashSet;
+import java.util.Set;
 import com.google.gwt.core.ext.typeinfo.JClassType;
-import com.vaadin.client.ui.button.ButtonConnector;
-import com.vaadin.client.ui.csslayout.CssLayoutConnector;
-import com.vaadin.client.ui.label.LabelConnector;
-import com.vaadin.client.ui.orderedlayout.HorizontalLayoutConnector;
-import com.vaadin.client.ui.orderedlayout.VerticalLayoutConnector;
-import com.vaadin.client.ui.panel.PanelConnector;
-import com.vaadin.client.ui.passwordfield.PasswordFieldConnector;
-import com.vaadin.client.ui.textfield.TextFieldConnector;
 import com.vaadin.client.ui.ui.UIConnector;
-import com.vaadin.client.ui.window.WindowConnector;
 import com.vaadin.server.widgetsetutils.ConnectorBundleLoaderFactory;
 import com.vaadin.shared.ui.Connect.LoadStyle;
 
-import java.util.HashSet;
-import java.util.Set;
-
-public final class OptimizedConnectorBundleLoaderFactory extends
+public class OptimizedConnectorBundleLoaderFactory extends
         ConnectorBundleLoaderFactory {
-    private final Set<String> eagerConnectors = new HashSet<String>();
+    private Set<String> eagerConnectors = new HashSet<String>();
     {
-        eagerConnectors.add(PasswordFieldConnector.class.getName());
-        eagerConnectors.add(VerticalLayoutConnector.class.getName());
-        eagerConnectors.add(HorizontalLayoutConnector.class.getName());
-        eagerConnectors.add(ButtonConnector.class.getName());
-        eagerConnectors.add(UIConnector.class.getName());
-        eagerConnectors.add(CssLayoutConnector.class.getName());
-        eagerConnectors.add(TextFieldConnector.class.getName());
-        eagerConnectors.add(PanelConnector.class.getName());
-        eagerConnectors.add(LabelConnector.class.getName());
-        eagerConnectors.add(WindowConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.ui.UIConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.orderedlayout.VerticalLayoutConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.label.LabelConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.extensions.ResponsiveConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.csslayout.CssLayoutConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.button.ButtonConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.orderedlayout.HorizontalLayoutConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.checkbox.CheckBoxConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.textfield.TextFieldConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.passwordfield.PasswordFieldConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.panel.PanelConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.menubar.MenuBarConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.textarea.TextAreaConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.customcomponent.CustomComponentConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.SingleSelectionModelConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.GridConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.data.DataCommunicatorConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.EditorConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.TextRendererConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.ColumnConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.connectors.grid.DetailsManagerConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.tabsheet.TabsheetConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.formlayout.FormLayoutConnector.class.getName());
     }
 
     @Override
-    protected LoadStyle getLoadStyle(final JClassType connectorType) {
+    protected LoadStyle getLoadStyle(JClassType connectorType) {
         if (eagerConnectors.contains(connectorType.getQualifiedBinaryName())) {
             return LoadStyle.EAGER;
         } else {
