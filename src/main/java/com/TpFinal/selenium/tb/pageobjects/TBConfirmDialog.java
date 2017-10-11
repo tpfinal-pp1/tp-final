05868827b70216923ba9a0ffafb97f2c79cfda22
Agus Alexander <agusalexander@hotmail.com>
2017-10-11 01:02:52 -0300

    agregados tests vaadin testbench

---
+++ src/main/java/com/TpFinal/selenium/tb/pageobjects/TBConfirmDialog.java
@@ -0,0 +1,24 @@
+package com.TpFinal.selenium.tb.pageobjects;
+
+import com.TpFinal.view.dummy.reports.ReportsView;
+import org.openqa.selenium.WebDriver;
+
+
+import com.vaadin.testbench.TestBenchTestCase;
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.WindowElement;
+
+public class TBConfirmDialog extends TestBenchTestCase {
+
+    private final WindowElement scope;
+
+    public TBConfirmDialog(WebDriver driver) {
+        setDriver(driver);
+        scope = $(WindowElement.class).id(ReportsView.CONFIRM_DIALOG_ID);
+    }
+
+    public void discard() {
+        $(ButtonElement.class).caption("Discard Changes").first().click();
+    }
+
+}
