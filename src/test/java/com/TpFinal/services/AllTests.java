663ad17e466061ee2ff2647bc52fdb71131f6371
roberGzt <rober.gerszen@gmail.com>
2017-10-12 19:42:25 -0300

    minor fix de la suite de tests.

--- src/test/java/com/TpFinal/services/AllTests.java
+++ src/test/java/com/TpFinal/services/AllTests.java
@@ -5,8 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({PersonaServiceNuevoTest.class, ContratoServiceTest.class, ProvinciaServiceTest.class, InmuebleServicePruebasTest.class, InmuebleServiceTest.class,
-	PublicacionServiceTest.class})
+@SuiteClasses({ PersonaServiceNuevoTest.class, ContratoServiceTest.class, ProvinciaServiceTest.class,
+	InmuebleServicePruebasTest.class, InmuebleServiceTest.class,
+	PublicacionServiceTest.class })
 public class AllTests {
-	
+
 }
