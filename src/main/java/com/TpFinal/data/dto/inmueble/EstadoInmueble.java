02219d8abb076e9b61cfde6ca99189684d206e8c
roberGzt <rober.gerszen@gmail.com>
2017-10-12 19:42:06 -0300

    añadido toString()

--- src/main/java/com/TpFinal/data/dto/inmueble/EstadoInmueble.java
+++ src/main/java/com/TpFinal/data/dto/inmueble/EstadoInmueble.java
@@ -1,5 +1,29 @@
 package com.TpFinal.data.dto.inmueble;
 
-public enum EstadoInmueble {Alquilado,EnAlquiler,EnVenta,NoPublicado,Vendido
-
+public enum EstadoInmueble {Alquilado,EnAlquiler,EnVenta,NoPublicado,Vendido;
+     @Override
+     public String toString(){
+	 String ret ="";
+	 switch (this) {
+	case Alquilado:
+	    ret = "Alquilado";
+	    break;
+	case EnAlquiler:
+	    ret = "En Alquiler";
+	    break;
+	case EnVenta:
+	    ret = "En Venta";
+	    break;
+	case Vendido:
+	    ret = "Vendido";
+	    break;	
+	case NoPublicado:
+	    ret = "No Publicado";
+	    break;	
+	default:
+	    ret = "-";
+	    break;
+	}
+	 return ret;
+     }
 }
