452654d7260808ddcd92b2bf87ad1bb5deede7dc
deidelson <deidelson93@gmail.com>
2017-10-14 17:31:07 -0300

    sarasa

--- src/test/java/com/TpFinal/data/dao/DAOContratoVentaTest.java
+++ src/test/java/com/TpFinal/data/dao/DAOContratoVentaTest.java
@@ -3,6 +3,7 @@ package com.TpFinal.data.dao;
 import com.TpFinal.data.conexion.ConexionHibernate;
 import com.TpFinal.data.conexion.TipoConexion;
 import com.TpFinal.data.dao.interfaces.DAOContratoVenta;
+import com.TpFinal.data.dao.interfaces.DAOInmueble;
 import com.TpFinal.data.dto.contrato.ContratoVenta;
 import com.TpFinal.data.dto.inmueble.ClaseInmueble;
 import com.TpFinal.data.dto.inmueble.Coordenada;
@@ -31,6 +32,7 @@ import static org.junit.Assert.*;
 
 public class DAOContratoVentaTest {
     DAOContratoVenta dao;
+    DAOInmueble daoInmuebles;
     List<ContratoVenta> contratos = new ArrayList<>();
 
     @BeforeClass
@@ -41,88 +43,104 @@ public class DAOContratoVentaTest {
     @Before
     public void setUp() throws Exception {
 
-	File dir = new File("Files");
-	deleteDirectory(dir);
-	dir.mkdir();
-	dao = new DAOContratoVentaImpl();
-	contratos.clear();
+		File dir = new File("Files");
+		deleteDirectory(dir);
+		dir.mkdir();
+		dao = new DAOContratoVentaImpl();
+		daoInmuebles=new DAOInmuebleImpl();
+		contratos = dao.readAll();
+		contratos.forEach(dao::delete);
+		contratos.clear();
     }
 
     @After
     public void tearDown() throws Exception {
-	contratos = dao.readAll();
-	contratos.forEach(dao::delete);
-	deleteDirectory(new File("Files"));
+		contratos = dao.readAll();
+		desvincularInmuebleYContrato();
+		contratos.forEach(dao::delete);
+		deleteDirectory(new File("Files"));
     }
+    
+    private void desvincularInmuebleYContrato() {
+    	daoInmuebles.readAll().forEach(inmueble ->{
+    	    inmueble.setContratos(null);
+    	    daoInmuebles.saveOrUpdate(inmueble);
+    	});
+    	dao.readAll().forEach(contrato -> {
+    	    contrato.setInmueble(null);
+    	    dao.saveOrUpdate(contrato);
+    	});
+    	
+        }
 
     @Test
     public void agregarSinDocs() {
-	dao.save(instancia("1.00"));
-	dao.save(instancia("2.00"));
-	dao.save(instancia("3.00"));
-
-	assertEquals(3, dao.readAll().size());
-	assertEquals(instanciaOV().getFechaPublicacion(), dao.readAll().get(0).getPublicacionVenta()
-		.getFechaPublicacion());
+		dao.save(instancia("1.00"));
+		dao.save(instancia("2.00"));
+		dao.save(instancia("3.00"));
+	
+		assertEquals(3, dao.readAll().size());
+		assertEquals(instanciaOV().getFechaPublicacion(), dao.readAll().get(0).getPublicacionVenta()
+			.getFechaPublicacion());
     }
 
     @Test
     public void delete() {
-	dao.save(instancia("1.00"));
-	dao.save(instancia("2.00"));
-	dao.save(instancia("3.00"));
-
-	dao.delete(dao.readAll().get(0));
-	assertEquals(dao.readAll().size(), 2);
-    }
-
-    @Test
-    public void update() {
-	dao.save(instancia("1.00"));
-	dao.save(instancia("2.00"));
-	dao.save(instancia("3.00"));
-
-	dao.readAll().forEach(contrato -> {
-	    contrato.setPrecioVenta(new BigDecimal("10.00"));
-	    dao.update(contrato);
-	});
-
-	dao.readAll().forEach(contrato -> {
-	    assertEquals(new BigDecimal("10.00"), contrato.getPrecioVenta());
-	});
+		dao.save(instancia("1.00"));
+		dao.save(instancia("2.00"));
+		dao.save(instancia("3.00"));
+	
+		dao.delete(dao.readAll().get(0));
+		assertEquals(dao.readAll().size(), 2);
+	    }
+	
+	    @Test
+	    public void update() {
+		dao.save(instancia("1.00"));
+		dao.save(instancia("2.00"));
+		dao.save(instancia("3.00"));
+	
+		dao.readAll().forEach(contrato -> {
+		    contrato.setPrecioVenta(new BigDecimal("10.00"));
+		    dao.update(contrato);
+		});
+	
+		dao.readAll().forEach(contrato -> {
+		    assertEquals(new BigDecimal("10.00"), contrato.getPrecioVenta());
+		});
 
     }
 
     @Test
     public void altaConDoc() throws SQLException, IOException {
-	String pathOriginal = "Files" + File.separator + "demo1.pdf";
-	File archivoOriginal = new File(pathOriginal);
-	archivoOriginal.createNewFile(); // lo creo
-	dao.saveOrUpdateContrato(instancia("1"), archivoOriginal);
-	assertEquals(1, dao.readAll().size());
-	ContratoVenta contratoPersistido = dao.readAll().get(0); // Lo traigo de DB
-	String pathPersistido = "Files" + File.separator + "demo2.pdf";
-	guardar(pathPersistido, blobToBytes(contratoPersistido.getDocumento())); // Lo escribo en disco de nuevo
-	File archivoPersistido = new File(pathPersistido);
-	assertTrue(archivoPersistido.exists());
-	assertTrue(FileUtils.contentEquals(archivoOriginal, archivoPersistido));
+		String pathOriginal = "Files" + File.separator + "demo1.pdf";
+		File archivoOriginal = new File(pathOriginal);
+		archivoOriginal.createNewFile(); // lo creo
+		dao.saveOrUpdateContrato(instancia("1"), archivoOriginal);
+		assertEquals(1, dao.readAll().size());
+		ContratoVenta contratoPersistido = dao.readAll().get(0); // Lo traigo de DB
+		String pathPersistido = "Files" + File.separator + "demo2.pdf";
+		guardar(pathPersistido, blobToBytes(contratoPersistido.getDocumento())); // Lo escribo en disco de nuevo
+		File archivoPersistido = new File(pathPersistido);
+		assertTrue(archivoPersistido.exists());
+		assertTrue(FileUtils.contentEquals(archivoOriginal, archivoPersistido));
     }
 
     @Test
     public void testRelacionInmueble() {
-	DAOInmuebleImpl daoI = new DAOInmuebleImpl();
-	Inmueble i = unInmuebleNoPublicado();
-	ContratoVenta c = instancia("1");
-
-	daoI.create(i);
-	c.setInmueble(i);
-	i.addContrato(c);
-	dao.saveOrUpdate(c);
-	daoI.saveOrUpdate(i);
-
-	assertEquals(1, daoI.readAll().get(0).getContratos().size());
-	assertEquals(i, dao.readAll().get(0).getInmueble());
-	daoI.readAll().forEach(daoI::delete);
+		DAOInmuebleImpl daoI = new DAOInmuebleImpl();
+		Inmueble i = unInmuebleNoPublicado();
+		ContratoVenta c = instancia("1");
+	
+		daoI.create(i);
+		c.setInmueble(i);
+		i.addContrato(c);
+		dao.saveOrUpdate(c);
+		daoI.saveOrUpdate(i);
+	
+		assertEquals(1, daoI.readAll().get(0).getContratos().size());
+		assertEquals(i, dao.readAll().get(0).getInmueble());
+		daoI.readAll().forEach(daoI::delete);
 
     }
 
