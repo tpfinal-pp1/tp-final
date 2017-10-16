a62fae7875be7ff63a26099a3b3b01872aa9507d
Agus Alexander <agusalexander@hotmail.com>
2017-10-16 18:33:07 -0300

    agregado Sonar a cache Travis

--- src/main/java/com/TpFinal/selenium/tb/MainViewIT.java
+++ src/main/java/com/TpFinal/selenium/tb/MainViewIT.java
@@ -3,6 +3,9 @@ package com.TpFinal.selenium.tb;
 import com.TpFinal.selenium.tb.pageobjects.TBLoginView;
 import com.TpFinal.selenium.tb.pageobjects.TBMainView;
 import com.TpFinal.selenium.tb.pageobjects.TBProfileWindow;
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.LabelElement;
+import com.vaadin.testbench.elements.TextFieldElement;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,16 +13,29 @@ import org.junit.Test;
 
 
 import com.vaadin.testbench.TestBenchTestCase;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class MainViewIT extends TestBenchTestCase {
 
     private TBLoginView loginView;
     private TBMainView mainView;
 
-    @Before
-    public void setUp() {
-        loginView = TBUtils.openInitialView();
-        mainView = loginView.login();
+
+
+
+    @Test
+    public void simpleTest() {
+        setDriver(new PhantomJSDriver());
+        getDriver().get("http://localhost:8080/");
+
+        TextFieldElement nameInput = $(TextFieldElement.class).first();
+        nameInput.setValue("Jeff");
+        ButtonElement helloButton = $(ButtonElement.class).caption("Click Me")
+                .first();
+        helloButton.click();
+
+        LabelElement outputLabel = $(LabelElement.class).first();
+        Assert.assertEquals("Thanks Jeff, it works!", outputLabel.getText());
     }
 
     @Test
@@ -30,8 +46,5 @@ public class MainViewIT extends TestBenchTestCase {
         Assert.assertEquals("Test User", mainView.getUserFullName());
     }
 
-    @After
-    public void tearDown() {
-        loginView.getDriver().quit();
-    }
+
 }
