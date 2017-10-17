4eaaeb19695dda2008f91fefb4f9c2dc12ee9ad0
Agus Alexander <agusalexander@hotmail.com>
2017-10-17 01:31:58 -0300

    fix?

--- src/main/java/com/TpFinal/data/conexion/TipoConexion.java
+++ src/main/java/com/TpFinal/data/conexion/TipoConexion.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Environment;
 
 public enum TipoConexion {	H2Test("org.hibernate.dialect.H2Dialect"
 									,"org.h2.Driver"
-									,"jdbc:h2:~//Test2;AUTO_SERVER=TRUE"
+									,"jdbc:h2:~//Test3;AUTO_SERVER=TRUE"
 									,"root"
 									,"root"
 									,"false"
