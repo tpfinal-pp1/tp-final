f9ebd54efd54d8f337b065addc5ba0ac98080dcf
Agus Alexander <agusalexander@hotmail.com>
2017-10-17 04:07:24 -0300

    intento numero 9999

--- .travis.script.sh
+++ .travis.script.sh
@@ -7,22 +7,20 @@
 
 
 #SI ES UN PULL REQUEST
+#SI ES UN PULL REQUEST
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
-	#SOLO UNIT TESTS PARA ACELERAR PROCESO
-	mvn  mvn clean test
-
-#SI NO ES UN PULL REQUEST Y ES EN MASTER(una vez mergeado)
+    mvn  mvn clean test #SOLO UNIT TESTS PARA ACELERAR PROCESO
+    #SI NO ES UN PULL REQUEST Y ES EN MASTER(una vez mergeado)
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
-then
-	# TEST DE INTEGRACION Y REPORTES COMPLETOS
-	mvn -B -e -V -Dvaadin.testbench.developer.license=dfe12f22-5ae4-4414-97a9-2e8474b8336c clean verify jacoco:report coveralls:report sonar:sonar
-
+then # TEST DE INTEGRACION Y REPORTES COMPLETOS
+    mvn -B -e -V -Dvaadin.testbench.developer.license=dfe12f22-5ae4-4414-97a9-2e8474b8336c clean verify jacoco:report coveralls:report sonar:sonar
 ##SI NO ES UN PULL REQUEST Y NO ES EN MASTER (cualquier otro commit a otra branch)
 elif [ "$TRAVIS_PULL_REQUEST"  == "false" ] && [ "${TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
-then
-	# SOLO TEST DE INTEGRACION
-	 mvn -B -e -V -Dvaadin.testbench.developer.license=dfe12f22-5ae4-4414-97a9-2e8474b8336c clean verify
+then #SOLO TEST DE INTEGRACION
+    mvn -B -e -V -Dvaadin.testbench.developer.license=dfe12f22-5ae4-4414-97a9-2e8474b8336c clean verify
+
+
 else
-      echo "No buildea"
+     echo "No buildea"
 fi
