44f343870c4411dae7db63294d6a316129f3c5fb
roberGzt <rober.gerszen@gmail.com>
2017-10-17 14:52:20 -0300

    añadido suit AllTest en com.TpFinal

---
+++ src/test/java/com/TpFinal/UnitTests/AllUtilsTest.java
@@ -0,0 +1,11 @@
+package com.TpFinal.UnitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ProvinciaServiceTest.class, UtilsTest.class })
+public class AllUtilsTest {
+
+}
