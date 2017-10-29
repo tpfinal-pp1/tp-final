a2b3ea810a5b3336ed7951c41f8ff1524e162a56
deidelson <deidelson93@gmail.com>
2017-10-29 11:57:17 -0300

    Update de tests

--- src/test/java/com/TpFinal/Integracion/dao/AllTestsDAO.java
+++ src/test/java/com/TpFinal/Integracion/dao/AllTestsDAO.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ DAOCobroImplIT.class, DAOContratoAlquilerIT.class, DAOContratoImplIT.class, DAOContratoVentaIT.class,
-	DAOInmuebleImplIT.class, DAOPersonaImplIT.class, DAOCitaImplIT.class,DAORecordatorioImplIT.class })
+	DAOInmuebleImplIT.class, DAOPersonaImplIT.class, DAOCitaImplIT.class,DAORecordatorioImplIT.class, DAOCredencialImplIT.class })
 public class AllTestsDAO {
 
 }
---
+++ src/test/java/com/TpFinal/Integracion/services/CredencialServiceIT.java
@@ -0,0 +1,14 @@
+package com.TpFinal.Integracion.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CredencialServiceIT {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
