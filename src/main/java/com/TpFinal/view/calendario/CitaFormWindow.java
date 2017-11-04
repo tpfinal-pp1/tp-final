50f75401c23072380d028ce4a5faefd4002f4ceb
Agus Alexander <agusalexander@hotmail.com>
2017-11-04 00:23:13 -0300

    fix para jenkins

--- src/main/java/com/TpFinal/view/calendario/CitaFormWindow.java
+++ src/main/java/com/TpFinal/view/calendario/CitaFormWindow.java
@@ -2,15 +2,14 @@ package com.TpFinal.view.calendario;
 
 import com.TpFinal.dto.cita.Cita;
 import com.TpFinal.dto.cita.TipoCita;
-import com.TpFinal.dto.contrato.Contrato;
+
 import com.TpFinal.dto.persona.Empleado;
-import com.TpFinal.dto.persona.Persona;
+
 import com.TpFinal.services.CitaService;
 import com.TpFinal.view.component.DeleteButton;
 import com.vaadin.data.Binder;
 import com.vaadin.data.ValidationException;
-import com.vaadin.data.validator.EmailValidator;
-import com.vaadin.data.validator.RegexpValidator;
+
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.Page;
@@ -22,8 +21,7 @@ import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.themes.ValoTheme;
-import javafx.scene.control.DatePicker;
-import org.apache.lucene.document.DateTools;
+
 
 import java.time.ZoneId;
 import java.util.ArrayList;
