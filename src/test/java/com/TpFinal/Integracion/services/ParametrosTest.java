fddce73b9b9ea897d173fc4a30d98cf5abfcb048
Agus Alexander <agusalexander@hotmail.com>
2017-11-14 19:57:39 -0300

    agregada funcion dowloadGStaticMapsWithMarker

--- src/test/java/com/TpFinal/Integracion/services/ParametrosTest.java
+++ src/test/java/com/TpFinal/Integracion/services/ParametrosTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 import com.TpFinal.properties.Parametros;
 
-public class ParametrosIT {
+public class ParametrosTest {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void illegar() throws IllegalArgumentException, FileExistsException {
 		Parametros.getProperty("sarasa");
 	}
-	
+
 	@Test 
 	public void traeBien() throws IllegalArgumentException, FileExistsException {
 		assertNotEquals(null, Parametros.getProperty("sendgrid.api.key"));
