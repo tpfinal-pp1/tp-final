cdf4321aff5567668b68fb14ded147791d5b3263
deidelson <deidelson93@gmail.com>
2017-11-14 20:13:07 -0300

    Agregado movimiento a partir de ventas

--- src/main/java/com/TpFinal/services/MovimientoService.java
+++ src/main/java/com/TpFinal/services/MovimientoService.java
@@ -16,87 +16,86 @@ import com.TpFinal.dto.movimiento.TipoMovimiento;
 
 public class MovimientoService {
 	DAOMovimiento dao;
-	
+
 	public MovimientoService() {
 		dao = new DAOMovimientoImpl();
 	}
-	
+
 	public boolean saveOrUpdate(Movimiento m) {
 		return dao.saveOrUpdate(m);
-	    }
+	}
 
-	    public boolean delete(Movimiento m) {
+	public boolean delete(Movimiento m) {
 		return dao.logicalDelete(m);
-	    }
+	}
 
-	    public List<Movimiento> readAll() {
+	public List<Movimiento> readAll() {
 		return dao.readAllActives();
-	    }
-	    
-	    public static Movimiento getInstanciaOtro() {
-	    	return new Movimiento.Builder()
-	    		.setdescripcionMovimiento("")
-	    		.setMonto(BigDecimal.ZERO)
-	    		.setFecha(LocalDate.now())
-	    		.setClaseMovimiento(ClaseMovimiento.Otro)
-	    		.setEstadoRegistro(EstadoRegistro.ACTIVO)
-	    		.setTipoMovimiento(TipoMovimiento.Ingreso)
-	    		.build();
-	        }
-	    
-	    public static Movimiento getInstanciaPagoAlquiler(Cobro c) {
-	    	ContratoAlquiler c1=(ContratoAlquiler) c.getContrato();
-	    	Movimiento ret= new Movimiento.Builder()
-	    		.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
-	    		.setMonto(c.getMontoRecibido())
-	    		.setFecha(LocalDate.now())
-	    		.setClaseMovimiento(ClaseMovimiento.Alquiler)
-	    		.setEstadoRegistro(EstadoRegistro.ACTIVO)
-	    		.setTipoMovimiento(TipoMovimiento.Ingreso)
-	    		.setContratoAlquiler(c1)
-	    		.build();
-	    	return ret;
-	        }
-	    
-	    public static Movimiento getInstanciaGananciaInmobiliaria(Cobro c) {
-	    	ContratoAlquiler c1=(ContratoAlquiler) c.getContrato();
-	    	Movimiento ret= new Movimiento.Builder()
-	    			.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
-		    		.setMonto(c.getComision())
-		    		.setFecha(LocalDate.now())
-		    		.setClaseMovimiento(ClaseMovimiento.Comisión)
-		    		.setEstadoRegistro(EstadoRegistro.ACTIVO)
-		    		.setTipoMovimiento(TipoMovimiento.Ingreso)
-		    		.setContratoAlquiler(c1)
-	    		.build();
-	    	return ret;
-	        }
-	    
-	    public static Movimiento getInstanciaPagoAPropietario(Cobro c) {
-	    	ContratoAlquiler c1=(ContratoAlquiler) c.getContrato();
-	    	Movimiento ret= new Movimiento.Builder()
-	    			.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
-		    		.setMonto(c.getMontoPropietario())
-		    		.setFecha(LocalDate.now())
-		    		.setClaseMovimiento(ClaseMovimiento.PagoAPropietario)
-		    		.setEstadoRegistro(EstadoRegistro.ACTIVO)
-		    		.setTipoMovimiento(TipoMovimiento.Egreso)
-		    		.setContratoAlquiler(c1)
-	    		.build();
-	    	return ret;
-	        }
-	    
-	    public static Movimiento getInstanciaPagoVenta(ContratoVenta contratoVenta) {
-	    	return new Movimiento.Builder()
-	    			.setdescripcionMovimiento(contratoVenta.getInmueble().toString())
-		    		.setMonto(contratoVenta.getPrecioVenta())
-		    		.setFecha(LocalDate.now())
-		    		.setClaseMovimiento(ClaseMovimiento.Venta)
-		    		.setEstadoRegistro(EstadoRegistro.ACTIVO)
-		    		.setTipoMovimiento(TipoMovimiento.Ingreso)
-		    		.setContratoVenta(contratoVenta)
-	    		.build();
-	        }
+	}
+
+	public static Movimiento getInstanciaOtro() {
+		return new Movimiento.Builder()
+				.setdescripcionMovimiento("")
+				.setMonto(BigDecimal.ZERO)
+				.setFecha(LocalDate.now())
+				.setClaseMovimiento(ClaseMovimiento.Otro)
+				.setEstadoRegistro(EstadoRegistro.ACTIVO)
+				.setTipoMovimiento(TipoMovimiento.Ingreso)
+				.build();
+	}
+
+	public static Movimiento getInstanciaPagoAlquiler(Cobro c) {
+		Movimiento ret= new Movimiento.Builder()
+				.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
+				.setMonto(c.getMontoRecibido())
+				.setFecha(LocalDate.now())
+				.setClaseMovimiento(ClaseMovimiento.Alquiler)
+				.setEstadoRegistro(EstadoRegistro.ACTIVO)
+				.setTipoMovimiento(TipoMovimiento.Ingreso)
+				.setCobro(c)
+				.build();
+		return ret;
+	}
+
+	public static Movimiento getInstanciaPagoVenta(Cobro c) {
+		Movimiento ret= new Movimiento.Builder()
+				.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
+				.setMonto(c.getMontoRecibido())
+				.setFecha(LocalDate.now())
+				.setClaseMovimiento(ClaseMovimiento.Venta)
+				.setEstadoRegistro(EstadoRegistro.ACTIVO)
+				.setTipoMovimiento(TipoMovimiento.Ingreso)
+				.setCobro(c)
+				.build();
+		return ret;
+	}
+
+	public static Movimiento getInstanciaGananciaInmobiliaria(Cobro c) {
+		Movimiento ret= new Movimiento.Builder()
+				.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
+				.setMonto(c.getComision())
+				.setFecha(LocalDate.now())
+				.setClaseMovimiento(ClaseMovimiento.Comisión)
+				.setEstadoRegistro(EstadoRegistro.ACTIVO)
+				.setTipoMovimiento(TipoMovimiento.Ingreso)
+				.setCobro(c)
+				.build();
+		return ret;
+	}
+
+	public static Movimiento getInstanciaPagoAPropietario(Cobro c) {
+		ContratoAlquiler c1=(ContratoAlquiler) c.getContrato();
+		Movimiento ret= new Movimiento.Builder()
+				.setdescripcionMovimiento(c.getContrato().getInmueble().toString())
+				.setMonto(c.getMontoPropietario())
+				.setFecha(LocalDate.now())
+				.setClaseMovimiento(ClaseMovimiento.PagoAPropietario)
+				.setEstadoRegistro(EstadoRegistro.ACTIVO)
+				.setTipoMovimiento(TipoMovimiento.Egreso)
+				.setCobro(c)
+				.build();
+		return ret;
+	}
 
 
 }
