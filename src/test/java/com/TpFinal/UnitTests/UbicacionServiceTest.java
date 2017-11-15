f59d9d40f195009c654ecbc5d81f3c0a045636bf
Agus Alexander <agusalexander@hotmail.com>
2017-11-14 21:40:16 -0300

    fix test

--- src/test/java/com/TpFinal/UnitTests/UbicacionServiceTest.java
+++ src/test/java/com/TpFinal/UnitTests/UbicacionServiceTest.java
@@ -2,6 +2,7 @@ package com.TpFinal.UnitTests;
 
 import com.TpFinal.dto.inmueble.Coordenada;
 import com.TpFinal.services.UbicacionService;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UbicacionServiceTest {
@@ -9,7 +10,8 @@ public class UbicacionServiceTest {
     @Test
     public void downloadMapImageTest(){
         UbicacionService uS=new UbicacionService();
-        uS.getMapImage(new Coordenada(-34.541461,+-58.715379));
+        Assert.assertNotNull(uS.getMapImage(new Coordenada(-34.541461,+-58.715379)));
+
 
     }
 }
