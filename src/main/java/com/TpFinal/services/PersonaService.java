e48707680c200d4b0268b041a2f5121e5025147f
deidelson <deidelson93@gmail.com>
2017-11-18 12:05:59 -0300

    Button para calcular calificaciones en personas

--- src/main/java/com/TpFinal/services/PersonaService.java
+++ src/main/java/com/TpFinal/services/PersonaService.java
@@ -26,292 +26,304 @@ import com.TpFinal.view.persona.FiltroInteresados;
 import com.itextpdf.text.log.SysoCounter;
 
 public class PersonaService {
-    DAOPersona dao;
-    CredencialService credencialService = new CredencialService();
+	DAOPersona dao;
+	CredencialService credencialService = new CredencialService();
 
-    public PersonaService() {
-	dao = new DAOPersonaImpl();
-    }
-
-    public boolean saveOrUpdate(Persona p) {
-	return dao.saveOrUpdate(p);
-    }
-
-    public boolean delete(Persona p) {
-	if (p.giveMeYourRoles().contains(Rol.Empleado)) {
-	    Credencial c = ((Empleado) p.getRol(Rol.Empleado)).getCredencial();
-	    if (c != null) {
-		credencialService.deepDelete(c);
-	    }
+	public PersonaService() {
+		dao = new DAOPersonaImpl();
 	}
-	return dao.logicalDelete(p);
-    }
 
-    public List<Persona> readAll() {
-	return dao.readAllActives();
-    }
+	public boolean saveOrUpdate(Persona p) {
+		return dao.saveOrUpdate(p);
+	}
 
-    public List<Persona> getInmobiliarias() {
-	return dao.readAllActives().stream()
-		.filter(p -> p.getEsInmobiliaria())
-		.collect(Collectors.toList());
-    }
+	public boolean delete(Persona p) {
+		if (p.giveMeYourRoles().contains(Rol.Empleado)) {
+			Credencial c = ((Empleado) p.getRol(Rol.Empleado)).getCredencial();
+			if (c != null) {
+				credencialService.deepDelete(c);
+			}
+		}
+		return dao.logicalDelete(p);
+	}
 
-    public List<Persona> getPersonas() {
-	return dao.readAllActives().stream()
-		.filter(p -> !p.getEsInmobiliaria())
-		.collect(Collectors.toList());
-    }
+	public List<Persona> readAll() {
+		return dao.readAllActives();
+	}
 
-    public List<Persona> getPropietariosQueNoSeanInmobiliarias() {
-	return findForRole(Rol.Propietario.toString()).stream()
-		.filter(p -> !p.getEsInmobiliaria())
-		.collect(Collectors.toList());
-    }
+	public List<Persona> getInmobiliarias() {
+		return dao.readAllActives().stream()
+				.filter(p -> p.getEsInmobiliaria())
+				.collect(Collectors.toList());
+	}
 
-    public Calificacion calificarInquilino(Persona p) {
-	Calificacion ret = null;
-	List<ContratoAlquiler> vigentes = getListaDeContratos(p);
-	if (p.getInquilino() != null && p.getInquilino().getContratos() != null && p.getInquilino().getContratos()
-		.size() != 0) {
-	    for (ContratoAlquiler ca : vigentes) {
-		List<Cobro> cobros = ca.getCobros().stream().collect(Collectors.toList());
-		cobros = this.filtrarUltimosSeisMeses(cobros);
-		new CobroService().calcularDatosFaltantes(cobros);
-		cobros.sort((c1, c2) -> c1.getFechaDeVencimiento().compareTo(c2.getFechaDeVencimiento()));
+	public List<Persona> getPersonas() {
+		return dao.readAllActives().stream()
+				.filter(p -> !p.getEsInmobiliaria())
+				.collect(Collectors.toList());
+	}
 
-		if (cantidadConsecutivos(cobros) >= 4) {
-		    p.getInquilino().setCalificacion(Calificacion.A);
-		    ret = Calificacion.A;
-		} else if (cantidadPagosAtrasados(cobros) > 0 && cantidadPagosAtrasados(cobros) <= 3
-			&& cantidadDeMesesComoInquilino(cobros) < 6) {
-		    p.getInquilino().setCalificacion(Calificacion.C);
-		    ret = Calificacion.C;
-		} else if (cantidadPagosAtrasados(cobros) >= 3) {
-		    p.getInquilino().setCalificacion(Calificacion.B);
-		    ret = Calificacion.B;
+	public List<Persona> getPropietariosQueNoSeanInmobiliarias() {
+		return findForRole(Rol.Propietario.toString()).stream()
+				.filter(p -> !p.getEsInmobiliaria())
+				.collect(Collectors.toList());
+	}
+
+	public Calificacion calificarInquilino(Persona p) {
+		Calificacion ret = null;
+		List<ContratoAlquiler> vigentes = getListaDeContratos(p);
+		if (p.getInquilino() != null && p.getInquilino().getContratos() != null && p.getInquilino().getContratos()
+				.size() != 0) {
+			int cantidadConsecutivos=0;
+			int cantidadPagosAtrasados=0;
+			int cantidadMesesComoInquilino=0;
+			
+			for (ContratoAlquiler ca : vigentes) {
+				List<Cobro> cobros = ca.getCobros().stream().collect(Collectors.toList());
+				cobros = this.filtrarUltimosSeisMeses(cobros);
+				new CobroService().calcularDatosFaltantes(cobros);
+				cobros.sort((c1, c2) -> c1.getFechaDeVencimiento().compareTo(c2.getFechaDeVencimiento()));
+				
+				if(cantidadConsecutivos(cobros)> cantidadConsecutivos)
+					cantidadConsecutivos=cantidadConsecutivos(cobros);
+				if(cantidadPagosAtrasados(cobros)> cantidadPagosAtrasados)
+					cantidadPagosAtrasados=cantidadPagosAtrasados(cobros);
+				if(cantidadMesesComoInquilino>  cantidadDeMesesComoInquilino(cobros))
+					cantidadMesesComoInquilino= cantidadDeMesesComoInquilino(cobros);
+			}	
+			
+			if (cantidadConsecutivos >= 4) {
+				p.getInquilino().setCalificacion(Calificacion.A);
+				ret = Calificacion.A;
+			} else if (cantidadPagosAtrasados > 0 && cantidadPagosAtrasados <= 3
+					&& cantidadPagosAtrasados < 6) {
+				p.getInquilino().setCalificacion(Calificacion.C);
+				ret = Calificacion.C;
+			} else if (cantidadPagosAtrasados >= 3) {
+				p.getInquilino().setCalificacion(Calificacion.B);
+				ret = Calificacion.B;
+			} else {
+				p.getInquilino().setCalificacion(Calificacion.D);
+				ret = Calificacion.D;
+			}
+			System.out.println("[Info] Calificacion calculada "+ret.toString());
+		}
+		return ret;
+	}
+
+	public List<ContratoAlquiler> getListaDeContratos(Persona p) {
+		List<ContratoAlquiler> ret = new ArrayList<>();
+		if (p.getInquilino() != null && p.getInquilino().getContratos() != null && p.getInquilino().getContratos()
+				.size() != 0) {
+			ret = p.getInquilino().getContratos().stream()
+					.filter(contrato -> contrato instanceof ContratoAlquiler)
+					.collect(Collectors.toList());
+		}
+		return ret;
+	}
+
+	public Integer cantidadPagosAtrasados(List<Cobro> c) {
+
+		List<Cobro> cobros = c.stream()
+				.filter(cob -> cobroAtrasado(cob))
+				.collect(Collectors.toList());
+
+		return cobros.size();
+	}
+
+	public Integer cantidadConsecutivos(List<Cobro> c) {
+		Integer cantidad = 0;
+		Integer aux = 0;
+		for (Cobro cobro : c) {
+			if (cobroAtrasado(cobro)) {
+				aux++;
+				if (aux > cantidad)
+					cantidad = aux;
+			} else
+				aux = 0;
+		}
+		return cantidad;
+	}
+
+	public boolean cobroAtrasado(Cobro c) {
+		boolean ret = false;
+		if (c.getFechaDeVencimiento().compareTo(LocalDate.now()) < 0) {
+			if (c.getFechaDePago() == null)
+				ret = true;
+			else if (c.getFechaDePago().compareTo(c.getFechaDeVencimiento()) > 0)
+				ret = true;
+		}
+		return ret;
+
+	}
+
+	public List<Cobro> filtrarUltimosSeisMeses(List<Cobro> cobros) {
+		LocalDate fecha = haceSeisMeses(cobros.get(0).getFechaDeVencimiento().getDayOfMonth());
+		return cobros.stream()
+				.filter(cob -> cob.getFechaDeVencimiento().compareTo(fecha) >= 0 && cob.getFechaDeVencimiento()
+				.compareTo(LocalDate.now()) <= 0)
+				.collect(Collectors.toList());
+	}
+
+	public Integer cantidadDeMesesComoInquilino(List<Cobro> cobros) {
+		LocalDate hoy = LocalDate.now();
+		hoy = hoy.withDayOfMonth(1);
+		LocalDate diaPrimerCobro = cobros.get(0).getFechaDeVencimiento();
+		diaPrimerCobro = diaPrimerCobro.withDayOfMonth(1);
+
+		return (int) ChronoUnit.MONTHS.between(hoy, diaPrimerCobro);
+	}
+
+	private LocalDate haceSeisMeses(Integer diaCobro) {
+		LocalDate fecha = LocalDate.now();
+		if (fecha.getDayOfMonth() > diaCobro) {
+			fecha = fecha.withDayOfMonth(1);
+			fecha = fecha.minusMonths(5);
 		} else {
-		    p.getInquilino().setCalificacion(Calificacion.D);
-		    ret = Calificacion.D;
-		}
-	    }
-	}
-	return ret;
-    }
-
-    public List<ContratoAlquiler> getListaDeContratos(Persona p) {
-	List<ContratoAlquiler> ret = new ArrayList<>();
-	if (p.getInquilino() != null && p.getInquilino().getContratos() != null && p.getInquilino().getContratos()
-		.size() != 0) {
-	    ret = p.getInquilino().getContratos().stream()
-	    		.filter(contrato -> contrato instanceof ContratoAlquiler)
-	    		.collect(Collectors.toList());
-	}
-	return ret;
-    }
-
-    public Integer cantidadPagosAtrasados(List<Cobro> c) {
-
-	List<Cobro> cobros = c.stream()
-		.filter(cob -> cobroAtrasado(cob))
-		.collect(Collectors.toList());
-
-	return cobros.size();
-    }
-
-    public Integer cantidadConsecutivos(List<Cobro> c) {
-	Integer cantidad = 0;
-	Integer aux = 0;
-	for (Cobro cobro : c) {
-	    if (cobroAtrasado(cobro)) {
-		aux++;
-		if (aux > cantidad)
-		    cantidad = aux;
-	    } else
-		aux = 0;
-	}
-	return cantidad;
-    }
-
-    public boolean cobroAtrasado(Cobro c) {
-	boolean ret = false;
-	if (c.getFechaDeVencimiento().compareTo(LocalDate.now()) < 0) {
-	    if (c.getFechaDePago() == null)
-		ret = true;
-	    else if (c.getFechaDePago().compareTo(c.getFechaDeVencimiento()) > 0)
-		ret = true;
-	}
-	return ret;
-
-    }
-
-    public List<Cobro> filtrarUltimosSeisMeses(List<Cobro> cobros) {
-	LocalDate fecha = haceSeisMeses(cobros.get(0).getFechaDeVencimiento().getDayOfMonth());
-	return cobros.stream()
-		.filter(cob -> cob.getFechaDeVencimiento().compareTo(fecha) >= 0 && cob.getFechaDeVencimiento()
-			.compareTo(LocalDate.now()) <= 0)
-		.collect(Collectors.toList());
-    }
-
-    public Integer cantidadDeMesesComoInquilino(List<Cobro> cobros) {
-	LocalDate hoy = LocalDate.now();
-	hoy = hoy.withDayOfMonth(1);
-	LocalDate diaPrimerCobro = cobros.get(0).getFechaDeVencimiento();
-	diaPrimerCobro = diaPrimerCobro.withDayOfMonth(1);
-
-	return (int) ChronoUnit.MONTHS.between(hoy, diaPrimerCobro);
-    }
-
-    private LocalDate haceSeisMeses(Integer diaCobro) {
-	LocalDate fecha = LocalDate.now();
-	if (fecha.getDayOfMonth() > diaCobro) {
-	    fecha = fecha.withDayOfMonth(1);
-	    fecha = fecha.minusMonths(5);
-	} else {
-	    fecha = fecha.withDayOfMonth(1);
-	    fecha = fecha.minusMonths(6);
-	}
-
-	return fecha;
-    }
-
-    public synchronized List<Persona> findAll(String stringFilter) {
-	ArrayList arrayList = new ArrayList();
-	List<Persona> personas = dao.readAllActives();
-	if (stringFilter != "") {
-
-	    for (Persona persona : personas) {
-
-		boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
-			|| persona.toString().toLowerCase()
-				.contains(stringFilter.toLowerCase());
-		if (passesFilter) {
-
-		    arrayList.add(persona);
+			fecha = fecha.withDayOfMonth(1);
+			fecha = fecha.minusMonths(6);
 		}
 
-	    }
-	} else {
-	    arrayList.addAll(personas);
+		return fecha;
 	}
 
-	Collections.sort(arrayList, new Comparator<Persona>() {
+	public synchronized List<Persona> findAll(String stringFilter) {
+		ArrayList arrayList = new ArrayList();
+		List<Persona> personas = dao.readAllActives();
+		if (stringFilter != "") {
 
-	    @Override
-	    public int compare(Persona o1, Persona o2) {
-		return (int) (o2.getId() - o1.getId());
-	    }
-	});
-	return arrayList;
-    }
+			for (Persona persona : personas) {
 
-    public synchronized List<Persona> findForRole(String stringFilter) {
-	ArrayList arrayList = new ArrayList();
-	List<Persona> personas = dao.readAllActives();
-	if (stringFilter != "") {
+				boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
+						|| persona.toString().toLowerCase()
+						.contains(stringFilter.toLowerCase());
+				if (passesFilter) {
 
-	    for (Persona persona : personas) {
+					arrayList.add(persona);
+				}
 
-		boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
-			|| persona.roles().toLowerCase()
-				.contains(stringFilter.toLowerCase());
-		if (passesFilter) {
-
-		    arrayList.add(persona);
+			}
+		} else {
+			arrayList.addAll(personas);
 		}
 
-	    }
-	} else {
-	    arrayList.addAll(personas);
+		Collections.sort(arrayList, new Comparator<Persona>() {
+
+			@Override
+			public int compare(Persona o1, Persona o2) {
+				return (int) (o2.getId() - o1.getId());
+			}
+		});
+		return arrayList;
 	}
 
-	Collections.sort(arrayList, new Comparator<Persona>() {
+	public synchronized List<Persona> findForRole(String stringFilter) {
+		ArrayList arrayList = new ArrayList();
+		List<Persona> personas = dao.readAllActives();
+		if (stringFilter != "") {
 
-	    @Override
-	    public int compare(Persona o1, Persona o2) {
-		return (int) (o2.getId() - o1.getId());
-	    }
-	});
-	return arrayList;
-    }
+			for (Persona persona : personas) {
 
-    public static Inquilino getPersonaConInquilino() {
-	Persona p = new Persona.Builder().setNombre("").setApellido("").build();
-	Inquilino i = new Inquilino.Builder().setPersona(p).build();
-	p.getRoles().add(i);
-	return i;
-    }
+				boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
+						|| persona.roles().toLowerCase()
+						.contains(stringFilter.toLowerCase());
+				if (passesFilter) {
 
-    public List<Persona> findAllClientes(FiltroInteresados filtro) {
-	List<Persona> personas = dao.readAllActives().stream()
-		.filter(p -> p.giveMeYourRoles().contains(Rol.Inquilino) || p.giveMeYourRoles().contains(
-			Rol.Propietario) || (!p.giveMeYourRoles().contains(Rol.Empleado)))
-		.filter(filtro.getFiltroCompuesto()).collect(Collectors.toList());
-	personas.sort(Comparator.comparing(Persona::getId));
-	return personas;
-    }
+					arrayList.add(persona);
+				}
 
-    public List<Empleado> findAllEmpleados(FiltroEmpleados filtro) {
-	List<Empleado> empleados = dao.readAll().stream()
-		.filter(p -> p.giveMeYourRoles().contains(Rol.Empleado))
-		.map(p -> (Empleado) p.getRol(Rol.Empleado))
-		.filter(filtro.getFiltroCompuesto())
-		.collect(Collectors.toList());
-	empleados.sort(Comparator.comparing(Empleado::getId));
-	return empleados;
-    }
-
-    public static Empleado getEmpleadoInstancia() {
-	Persona p = new Persona();
-	Empleado e = new Empleado.Builder()
-		.setFechaDeAlta(LocalDate.now())
-		.setPersona(p)
-		.build();
-	Credencial c = new Credencial.Builder().setEmpleado(e).build();
-	e.setCredencial(c);
-	return e;
-    }
-
-    public boolean darDeBajaEmpleado(Empleado empleado) {
-	boolean ret = true;
-
-	if (empleado.getCredencial() != null)
-	    credencialService.deepDelete(empleado.getCredencial());
-	empleado.setCredencial(null);
-	empleado.setFechaDeBaja(LocalDate.now());
-	empleado.setEstadoEmpleado(EstadoEmpleado.NOACTIVO);
-	ret = ret && dao.saveOrUpdate(empleado.getPersona());
-	ret = ret && this.delete(empleado.getPersona());
-	if (!ret) {
-	    // Rollback.
-	    empleado.setFechaDeBaja(null);
-	    empleado.setEstadoEmpleado(EstadoEmpleado.ACTIVO);
-	    empleado.getPersona().setEstadoRegistro(EstadoRegistro.ACTIVO);
-	    dao.saveOrUpdate(empleado.getPersona());
-	}
-
-	return ret;
-
-    }
-
-    public boolean reincorporarEmpleado(Empleado empleado) {
-	boolean ret = false;
-	if (empleado != null) {
-	    if (empleado.getPersona() != null) {
-		empleado.setEstadoEmpleado(EstadoEmpleado.ACTIVO);
-		LocalDate fechaAlta = empleado.getFechaDeAlta();
-		LocalDate fechaBaja = empleado.getFechaDeBaja();
-		empleado.setFechaDeAlta(LocalDate.now());
-		empleado.setFechaDeBaja(null);
-		ret = dao.saveOrUpdate(empleado.getPersona());
-		if (ret == false) {
-		    empleado.setEstadoEmpleado(EstadoEmpleado.NOACTIVO);
-		    empleado.setFechaDeAlta(fechaAlta);
-		    empleado.setFechaDeBaja(fechaBaja);
+			}
+		} else {
+			arrayList.addAll(personas);
 		}
-	    }
+
+		Collections.sort(arrayList, new Comparator<Persona>() {
+
+			@Override
+			public int compare(Persona o1, Persona o2) {
+				return (int) (o2.getId() - o1.getId());
+			}
+		});
+		return arrayList;
 	}
-	return ret;
-    }
+
+	public static Inquilino getPersonaConInquilino() {
+		Persona p = new Persona.Builder().setNombre("").setApellido("").build();
+		Inquilino i = new Inquilino.Builder().setPersona(p).build();
+		p.getRoles().add(i);
+		return i;
+	}
+
+	public List<Persona> findAllClientes(FiltroInteresados filtro) {
+		List<Persona> personas = dao.readAllActives().stream()
+				.filter(p -> p.giveMeYourRoles().contains(Rol.Inquilino) || p.giveMeYourRoles().contains(
+						Rol.Propietario) || (!p.giveMeYourRoles().contains(Rol.Empleado)))
+				.filter(filtro.getFiltroCompuesto()).collect(Collectors.toList());
+		personas.sort(Comparator.comparing(Persona::getId));
+		return personas;
+	}
+
+	public List<Empleado> findAllEmpleados(FiltroEmpleados filtro) {
+		List<Empleado> empleados = dao.readAll().stream()
+				.filter(p -> p.giveMeYourRoles().contains(Rol.Empleado))
+				.map(p -> (Empleado) p.getRol(Rol.Empleado))
+				.filter(filtro.getFiltroCompuesto())
+				.collect(Collectors.toList());
+		empleados.sort(Comparator.comparing(Empleado::getId));
+		return empleados;
+	}
+
+	public static Empleado getEmpleadoInstancia() {
+		Persona p = new Persona();
+		Empleado e = new Empleado.Builder()
+				.setFechaDeAlta(LocalDate.now())
+				.setPersona(p)
+				.build();
+		Credencial c = new Credencial.Builder().setEmpleado(e).build();
+		e.setCredencial(c);
+		return e;
+	}
+
+	public boolean darDeBajaEmpleado(Empleado empleado) {
+		boolean ret = true;
+
+		if (empleado.getCredencial() != null)
+			credencialService.deepDelete(empleado.getCredencial());
+		empleado.setCredencial(null);
+		empleado.setFechaDeBaja(LocalDate.now());
+		empleado.setEstadoEmpleado(EstadoEmpleado.NOACTIVO);
+		ret = ret && dao.saveOrUpdate(empleado.getPersona());
+		ret = ret && this.delete(empleado.getPersona());
+		if (!ret) {
+			// Rollback.
+			empleado.setFechaDeBaja(null);
+			empleado.setEstadoEmpleado(EstadoEmpleado.ACTIVO);
+			empleado.getPersona().setEstadoRegistro(EstadoRegistro.ACTIVO);
+			dao.saveOrUpdate(empleado.getPersona());
+		}
+
+		return ret;
+
+	}
+
+	public boolean reincorporarEmpleado(Empleado empleado) {
+		boolean ret = false;
+		if (empleado != null) {
+			if (empleado.getPersona() != null) {
+				empleado.setEstadoEmpleado(EstadoEmpleado.ACTIVO);
+				LocalDate fechaAlta = empleado.getFechaDeAlta();
+				LocalDate fechaBaja = empleado.getFechaDeBaja();
+				empleado.setFechaDeAlta(LocalDate.now());
+				empleado.setFechaDeBaja(null);
+				ret = dao.saveOrUpdate(empleado.getPersona());
+				if (ret == false) {
+					empleado.setEstadoEmpleado(EstadoEmpleado.NOACTIVO);
+					empleado.setFechaDeAlta(fechaAlta);
+					empleado.setFechaDeBaja(fechaBaja);
+				}
+			}
+		}
+		return ret;
+	}
 
 }
