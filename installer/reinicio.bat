0b52422941f6ac4e39152f2fc4ab5e0439882470
Agus Alexander <agusalexander@hotmail.com>
2017-11-24 17:30:37 -0300

    updateado Installer

--- installer/reinicio.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-:myLabel
-
-SET /P continue="Para Continuar Debe reinicar la PC,desa hacerlo ahora? (S/N): "
-IF %continue% EQU S (
-shutdown /r /t 00
-TIMEOUT /T 600 /NOBREAK
-GOTO myLabel
-)
